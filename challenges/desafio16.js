7dd0b0a3eaa6eb66d9166082b5271dbd2916c826
Wendril-Costa <wendrild.c.n@gmail.com>
2023-01-23 19:24:38 -0400

    16 a 20

---
+++ challenges/desafio16.js
@@ -0,0 +1,16 @@
+db.produtos.updateOne({
+    nome: "Big Mac",
+  }, {
+    $currentDate: {
+      ultimaModificacao: 1,
+    },
+  });
+  
+  db.produtos.find({
+    ultimaModificacao: {
+      $exists: 1,
+    },
+  }, {
+    _id: 0,
+    nome: 1,
+  });
